8cd543f65db886b3dafbc2abc9fcb94919bc1238
sirmaverick <sirmaverick>
2011-04-09 18:56:22 +0200

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Random Spring Content $VERSION',
+  description='Random Spring Content',
+  shortname='RSC',
+  version='$VERSION',
+  mutator='1',
+  game='',
+  shortGame='',
+  modtype=1,
+  depend = {
+    'Zero-K $VERSION',
+  },
+}
+
